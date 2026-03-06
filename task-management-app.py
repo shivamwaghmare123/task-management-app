512fa4c8f8d7099fb2946d3962608e630afaeec7
shivamwaghmare123 <194880723+shivamwaghmare123@users.noreply.github.com>
2026-03-06 17:29:55 +0530

    Add files via upload
    # Task Management App (Python CLI)
    
    A simple command-line Task Management Application built using Python.  
    This program allows users to manage daily tasks through a menu-driven interface.
    
    ## Features
    - Add new tasks
    - Update existing tasks
    - Delete tasks
    - View all tasks
    - Exit program
    
    ## Technologies Used
    - Python
    - Command Line Interface (CLI)
    - Git & GitHub
    
    ## How to Run
    
    1. Clone the repository
    
    git clone https://github.com/shivamwaghmare123/Task_managment_App.git
    
    2. Navigate to the project folder
    
    cd Task_managment_App
    
    3. Run the program
    
    python task.py
    
    ## Project Structure
    
    Task_managment_App
    │
    └── task.py
    
    ## Author
    Shivam Waghmare
    GitHub: https://github.com/shivamwaghmare123
    LinkedIn: https://www.linkedin.com/in/shivam-waghmare-862131324

---
+++ task-management-app.py
@@ -0,0 +1,55 @@
+def task():
+    tasks = []
+
+    print("----WELCOME TO THE TASK MANAGEMENT APP----")
+
+    total_task = int(input("Enter how many tasks you want to add = "))
+
+    for i in range(1, total_task + 1):
+        task_name = input(f"Enter task {i} = ")
+        tasks.append(task_name)
+
+    print(f"Today's tasks are {tasks}")
+
+    while True:
+
+        operation = int(input("Enter\n1-Add\n2-Update\n3-Delete\n4-View\n5-Exit\n"))
+
+        if operation == 1:
+            add = input("Enter task you want to add = ")
+            tasks.append(add)
+            print(f"Task '{add}' added successfully")
+
+        elif operation == 2:
+            updated_val = input("Enter task name you want to update = ")
+
+            if updated_val in tasks:
+                up = input("Enter new task = ")
+                ind = tasks.index(updated_val)
+                tasks[ind] = up
+                print("Task updated successfully")
+            else:
+                print("Task not found")
+
+        elif operation == 3:
+            del_val = input("Which task you want to delete = ")
+
+            if del_val in tasks:
+                ind = tasks.index(del_val)
+                del tasks[ind]
+                print("Task deleted successfully")
+            else:
+                print("Task not found")
+
+        elif operation == 4:
+            print(f"Total tasks: {tasks}")
+
+        elif operation == 5:
+            print("Closing the program...")
+            break
+
+        else:
+            print("Invalid input")
+
+
+task()
